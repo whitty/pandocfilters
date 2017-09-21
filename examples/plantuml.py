69ace93a2fe409b0c9476813b518e595a85f0f5e
Greg Whiteley <greg.whiteley@gmail.com>
2017-09-21 20:13:44 +1000

    Added backport of pandoc helper functions

--- examples/plantuml.py
+++ examples/plantuml.py
@@ -11,8 +11,82 @@ import os
 import sys
 from subprocess import call
 
-from pandocfilters import toJSONFilter, Para, Image, get_filename4code, get_caption, get_extension
+from pandocfilters import toJSONFilter, Para, Image
 
+#########################################################################
+# backported helpers
+
+# from pandocfilters import get_filename4code, get_caption, get_extension
+import pandocfilters
+# redirect names by defaulting to these if not in pandocfilters
+
+def backport_get_filename4code(module, content, ext=None):
+    """Generate filename based on content
+
+    The function ensures that the (temporary) directory exists, so that the
+    file can be written.
+
+    Example:
+        filename = get_filename4code("myfilter", code)
+    """
+    imagedir = module + "-images"
+    import hashlib
+    fn = hashlib.sha1(content.encode(sys.getfilesystemencoding())).hexdigest()
+    try:
+        os.mkdir(imagedir)
+        sys.stderr.write('Created directory ' + imagedir + '\n')
+    except OSError:
+        pass
+    if ext:
+        fn += "." + ext
+    return os.path.join(imagedir, fn)
+get_filename4code = getattr(pandocfilters, "get_filename4code", backport_get_filename4code)
+
+def backport_get_value(kv, key, value = None):
+    """get value from the keyvalues (options)"""
+    res = []
+    for k, v in kv:
+        if k == key:
+            value = v
+        else:
+            res.append([k, v])
+    return value, res
+get_value = getattr(pandocfilters, "get_value", backport_get_value)
+
+def backport_get_caption(kv):
+    """get caption from the keyvalues (options)
+
+    Example:
+      if key == 'CodeBlock':
+        [[ident, classes, keyvals], code] = value
+        caption, typef, keyvals = get_caption(keyvals)
+        ...
+        return Para([Image([ident, [], keyvals], caption, [filename, typef])])
+    """
+    caption = []
+    typef = ""
+    from pandocfilters import Str
+    value, res = get_value(kv, u"caption")
+    if value is not None:
+        caption = [Str(value)]
+        typef = "fig:"
+
+    return caption, typef, res
+get_caption = getattr(pandocfilters, "get_caption", backport_get_caption)
+
+def backport_get_extension(format, default, **alternates):
+    """get the extension for the result, needs a default and some specialisations
+
+    Example:
+      filetype = get_extension(format, "png", html="svg", latex="eps")
+    """
+    try:
+        return alternates[format]
+    except KeyError:
+        return default
+get_extension = getattr(pandocfilters, "get_extension", backport_get_extension)
+
+#########################################################################
 
 def plantuml(key, value, format, _):
     if key == 'CodeBlock':
@@ -37,7 +111,8 @@ def plantuml(key, value, format, _):
                 call(["java", "-jar", "plantuml.jar", "-t"+filetype, src])
                 sys.stderr.write('Created image ' + dest + '\n')
 
-            return Para([Image([ident, [], keyvals], caption, [dest, typef])])
+            return Para([Image(#[ident, [], keyvals],
+                caption, [dest, typef])])
 
 if __name__ == "__main__":
     toJSONFilter(plantuml)
